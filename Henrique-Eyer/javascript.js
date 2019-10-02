f8727ecfeef1c28596893ce1006166c358b49b3e
Henrique Eyer <henriqueeyer@outlook.com>
2019-10-01 22:53:54 -0300

    Criada função de escolher o tamanho da matriz, e botao onde reseta ela, apaga tudo, e adicionada funcionalidade ao input de numero para ele ser desabilitado quando a matriz estiver na tela e volta a ser ativado quando ela é deletada

--- Henrique-Eyer/javascript.js
+++ Henrique-Eyer/javascript.js
@@ -1,6 +1,69 @@
 
-var celulas=document.getElementsByTagName('td');
+var celulas=pegaMatriz();
+var btnclear=document.getElementById("btnclear");
+var tamanho=document.getElementById('tamanho');
+var table=document.querySelector('table');
 var selectcor="black";
+var cores=document.getElementById('cor1');
+var reset=document.getElementById('reset');
+
+window.addEventListener('load', function (){
+    selectcor=document.getElementById('cor1').style.background;
+    disablemargem(document.getElementById('cor1'));
+})
+
+
+tamanho.addEventListener('change',function(){
+    var i;
+    var j;
+    
+    for(i=0;i<tamanho.value;i++){
+        let linha=document.createElement("tr")
+        table.appendChild(linha);
+        
+        for(j=0;j<tamanho.value;j++){
+            let cel=document.createElement('td')
+            
+            linha.appendChild(cel)
+        }
+    }
+    
+    
+    funcparamatriz(pegaMatriz());
+    disable();
+})
+
+function disable(){
+    document.getElementById("tamanho").disabled = true;
+}
+function enable(){
+    document.getElementById("tamanho").disabled = false;
+}
+
+reset.addEventListener('click', function(){
+    let i;
+    for(i=0;i<tamanho.value;i++){
+        let childtable=table.firstElementChild
+        table.removeChild(childtable);
+    }
+    enable();
+})
+
+
+
+function pegaMatriz(){
+    let celulas=document.getElementsByTagName('td');
+    return celulas;
+}
+
+
+
+
+cores.style.background='black';
+cores.addEventListener('click', function(){
+    selectcor=this.style.background;
+    disablemargem(this);
+})
 
 
 
@@ -11,9 +74,9 @@ function disablemargem(item){
         let allcores=document.getElementById('cor'+i);
         allcores.style.boxShadow="";
     }
-
+    
     item.style.boxShadow=" 4px 4px "+item.style.background
-    console.log(item);
+    
 }
 
 
@@ -23,24 +86,11 @@ function gera_cor(){
     var cor = '#';
     // Pega um número aleatório no array acima
     for (var i = 0; i < 6; i++ ) {
-    //E concatena à variável cor
+        //E concatena à variável cor
         cor += hexadecimais[Math.floor(Math.random() * 16)];
     }
     return cor;
 }
-
-var cores=document.getElementById('cor1');
-cores.style.background='black';
-cores.addEventListener('click', function(){
-    selectcor=this.style.background;
-    disablemargem(this);
-})
-
-
-window.addEventListener('load', function (){
-    selectcor=document.getElementById('cor1').style.background;
-    disablemargem(document.getElementById('cor1'));
-})
 
 
 for(i=2;i<5;i++){
@@ -53,18 +103,19 @@ for(i=2;i<5;i++){
 }
 
 
-for(i=0;i<celulas.length;i++){
-    celulas[i].addEventListener('click', function (){
-        this.style.background=selectcor;
+function funcparamatriz(celulas){
+    for(i=0;i<celulas.length;i++){
+        celulas[i].addEventListener('click', function (){
+            this.style.background=selectcor;
+        })
+    }}
+    
+    btnclear.addEventListener('click', function(){
+        let i;
+        for(i=0;i<celulas.length;i++){
+            celulas[i].style.background="#ffffff";   
+        }
     })
-}
-
-
-var btnclear=document.getElementById("btnclear");
-btnclear.addEventListener('click', function(){
-    let i;
-for(i=0;i<celulas.length;i++){
-    celulas[i].style.background="#ffffff";   
-}
-})
-
+    
+    
+    
