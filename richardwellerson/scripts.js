647f0d462a7de1230ae33e7f396704faa60a5eb7
Richard Wellerson <richardwellerson@gmail.com>
2019-10-03 18:06:47 -0300

    Adicionando bases de cada documento

---
+++ richardwellerson/scripts.js
@@ -0,0 +1,8 @@
+valorRecebido = document.getElementsByTagName("input");
+
+for (var line = 0; line < valorRecebido; line++) {
+    var linha = document.createElement("tr");
+        for (var cell = 0; cell < valorRecebido; cell++) {
+            var celula = document.createElement("td");
+        }
+}
