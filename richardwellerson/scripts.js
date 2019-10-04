28bcb6304cfa27dde8e439dfc5fd5c09689ede0c
Richard Wellerson <richardwellerson@gmail.com>
2019-10-04 14:49:01 -0300

    Alternando var por let

--- richardwellerson/scripts.js
+++ richardwellerson/scripts.js
@@ -1,8 +1,14 @@
 valorRecebido = document.getElementsByTagName("input");
 
-for (var line = 0; line < valorRecebido; line++) {
-    var linha = document.createElement("tr");
-        for (var cell = 0; cell < valorRecebido; cell++) {
-            var celula = document.createElement("td");
-        }
+rodarTabela = if(valorRecebido < 5 && valorRecebido > 50){
+    alert("Valor Incorreto")
+
+    }else if (valorRecebido > 4 && valorRecebido < 51) {
+        for (let line = 0; line < valorRecebido; line++) {
+            let linha = document.createElement("tr");
+                for (let cell = 0; cell < valorRecebido; cell++) {
+                    let celula = document.createElement("td");
+            }
+    }
 }
+
