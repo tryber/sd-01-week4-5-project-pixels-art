5dc34cbdbf97f451b63832cff11dd56a9a27fcf0
Richard Wellerson <richardwellerson@gmail.com>
2019-10-04 17:41:08 -0300

    tabela adicionada, com id separado para cada

--- richardwellerson/scripts.js
+++ richardwellerson/scripts.js
@@ -1,14 +1,31 @@
-valorRecebido = document.getElementsByTagName("input");
+// valorRecebido = document.getElementsByTagName("input");
 
-rodarTabela = if(valorRecebido < 5 && valorRecebido > 50){
-    alert("Valor Incorreto")
+// function blurBolado () {
+//     document.getElementsByTagName("input").addEventListener("keyup",rodarTabela());
+// }
 
-    }else if (valorRecebido > 4 && valorRecebido < 51) {
-        for (let line = 0; line < valorRecebido; line++) {
-            let linha = document.createElement("tr");
-                for (let cell = 0; cell < valorRecebido; cell++) {
-                    let celula = document.createElement("td");
-            }
-    }
-}
+// function rodarTabela () {
+//     if(valorRecebido < 5 && valorRecebido > 50){
+//     alert("Valor Incorreto")
+
+//     } else if (valorRecebido > 4 && valorRecebido < 51) {
+//         for (let line = 0; line < valorRecebido; line++) {
+//             let linha = document.createElement("tr");
+//             let tabelaChamar = document.getElementsByClassName("tbljs")[0];
+//             tabelaChamar.appendChild(linha);
+//                 for (let cell = 0; cell < valorRecebido; cell++) {
+//                     let celula = document.createElement("td");
+                    
+//             }
+        
+//     }
+    
+// }
+
+// }
+
+let tblBlack = document.getElementById("tblblack");
+
+
+
 
