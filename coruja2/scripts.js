f4db0e0b77c91195acf89704eb8c2fa0004b1a7b
Gabriel Dalseco dos Santos <gabriel_santos96@outlook.com>
2019-10-07 18:18:45 -0300

    Projeto 2 finalizado

--- coruja2/scripts.js
+++ coruja2/scripts.js
@@ -1,11 +1,39 @@
-function draw() {
-    var canvas = document.getElementById('canvas');
-    if (canvas.getContext){
-      var ctx = canvas.getContext('2d');
-      ctx.beginPath();
-      ctx.moveTo(75,50);
-      ctx.lineTo(100,75);
-      ctx.lineTo(100,25);
-      ctx.fill();
-    }
-   }
+var color1 = document.getElementsByClassName("paleta1")[0]
+var color2 = document.getElementsByClassName("paleta2")[0]
+var color3 = document.getElementsByClassName("paleta3")[0]
+var color4 = document.getElementsByClassName("paleta4")[0]
+
+var selectColor = "black"
+pintartodasascores();
+
+color1.addEventListener('click', function(){
+  let computedStyle = getComputedStyle(color1);
+  selectColor = computedStyle.backgroundColor;
+})
+
+color2.addEventListener('click', function(){
+  let computedStyle = getComputedStyle(color2);
+  selectColor = computedStyle.backgroundColor;
+})
+
+color3.addEventListener('click', function(){
+  let computedStyle = getComputedStyle(color3);
+  selectColor = computedStyle.backgroundColor;
+})
+
+color4.addEventListener('click', function(){
+  let computedStyle = getComputedStyle(color4);
+  selectColor = computedStyle.backgroundColor; 
+})
+
+function pintartodasascores(){
+  let printando = document.getElementsByTagName("td")
+  for (let i = 0; i < printando.length ;i++){
+    printando[i].addEventListener('click', function(){
+    printando[i].style.background = selectColor;
+    })  
+  }
+}
+  
+  
+  
