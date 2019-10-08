1aa43c50bb8d31a2eedb9ad2671c07771b064df3
antoniopaolillo <antonio_francisco_p@live.com>
2019-10-08 10:47:25 -0300

    aperfeiçoando o script e html e adicionando informações ao README

--- antoniopaolillo/README.md
+++ antoniopaolillo/README.md
@@ -1,1 +1,7 @@
-instruções a seguir:
+Instruções a seguir:
+
+Este é um projeto realizado para o curso de Desenvolvimento de Software da TRYBE, em 10/2019.
+
+O projeto visa construir uma paleta de cores, que pode ser alterada selecionando a cor desejada e colorindo a tabela via clique. Outras funções também estão presentes no projeto, como alterar o tamanho do quadrado de pixels mediante um input, gerar um perfil github com a cor e tamanho selecionados e limpar a tabela.
+
+Para executar o projeto e analisar seu resultado, basta acessar: https://antoniopaolillo.github.io/projetos/projeto2js/antoniopaolillo/index.html
--- antoniopaolillo/script.js
+++ antoniopaolillo/script.js
@@ -1,5 +1,5 @@
 //gerando cores aleatorias
-function geradordecores() {
+function geradorDeCores() {
     for (let i = 0; i < 4; i++) {
         var cor1 = [];
         cor1[i] = document.getElementsByClassName("coresfixas")[i];
@@ -15,79 +15,53 @@ function geradordecores() {
 }
 
 //armazenando a cor ao clique, colocando numa variavel e alterando a cor do mostrador
-var corfinal = "black";
-var primeiracor = document.getElementsByClassName("coresfixas")[0];
-var segundacor = document.getElementsByClassName("coresfixas")[1];
-var terceiracor = document.getElementsByClassName("coresfixas")[2];
-var quartacor = document.getElementsByClassName("coresfixas")[3];
-var quintacor = document.getElementsByClassName("coresfixas")[4];
-var corselecionada = document.getElementsByClassName("corselecionada")[0];
-
-primeiracor.addEventListener('click', function () {
-    corfinal = primeiracor.style.backgroundColor;
-    corselecionada.style.backgroundColor = corfinal;
-})
-segundacor.addEventListener('click', function () {
-    corfinal = segundacor.style.backgroundColor;
-    corselecionada.style.backgroundColor = corfinal;
-})
-terceiracor.addEventListener('click', function () {
-    corfinal = terceiracor.style.backgroundColor;
-    corselecionada.style.backgroundColor = corfinal;
-})
-quartacor.addEventListener('click', function () {
-    corfinal = quartacor.style.backgroundColor;
-    corselecionada.style.backgroundColor = corfinal;
-})
-quintacor.addEventListener('click', function () {
-    corfinal = "black";
-    corselecionada.style.backgroundColor = corfinal;
-})
-
-//adicionando a cor ao elemento clicado
-var elementodatabela = document.getElementsByTagName('td');
-for (i = 0; i < elementodatabela.length; i++) {
-    elementodatabela[i].addEventListener('click', function () {
-        this.style.background = corfinal;
+var corFinal = "black";
+let corSelecionada = document.getElementsByClassName("corselecionada")[0];
+for (let i = 0; i < 5; i++) {
+    let corClicada = document.getElementsByClassName("coresfixas")[i];
+    corClicada.addEventListener('click', function () {
+        corFinal = corClicada.style.backgroundColor;
+        corSelecionada.style.backgroundColor = corFinal;
     })
 }
 
 //função criada para ser chamada novamente após mudar a tabela
-function adicionarcor() {
-    let elementodatabela2 = document.getElementsByTagName('td');
-    for (i = 0; i < elementodatabela2.length; i++) {
-        elementodatabela2[i].addEventListener('click', function () {
-            this.style.background = corfinal;
+function adicionarCor() {
+    let elementoDaTabela2 = document.getElementsByTagName('td');
+    for (i = 0; i < elementoDaTabela2.length; i++) {
+        elementoDaTabela2[i].addEventListener('click', function () {
+            this.style.background = corFinal;
         })
     }
 }
+adicionarCor();
 
 //limpando a tela
-function limpartela() {
-    let elementodatabela3 = document.getElementsByTagName('td');
-    for (i = 0; i < elementodatabela3.length; i++) {
-        elementodatabela3[i].style.background = "white";
+function limparTela() {
+    let elementoDaTabela3 = document.getElementsByTagName('td');
+    for (i = 0; i < elementoDaTabela3.length; i++) {
+        elementoDaTabela3[i].style.background = "white";
     }
 }
 
 //mudando o tamanho do quadrado 
-function tamanhoquadrado() {
+function tamanhoQuadrado() {
     let contador = document.getElementById("tamanhoquadrado").value;
     if (contador > 5 && contador < 51) {
-        var tamanhodatabela = document.getElementsByTagName("tr");
-        var segundocontador = tamanhodatabela.length - 1;
-        var tabelaapagar = document.getElementsByTagName('tbody')[1];
+        var tamanhoDaTabela = document.getElementsByTagName("tr");
+        var segundoContador = tamanhoDaTabela.length - 1;
+        var tabelaApagar = document.getElementsByTagName('tbody')[1];
         //agapando a table
-        for (let i = 0; i < segundocontador; i++) {
-            let apagar = tabelaapagar.firstElementChild;
-            tabelaapagar.removeChild(apagar);
+        for (let i = 0; i < segundoContador; i++) {
+            let apagar = tabelaApagar.firstElementChild;
+            tabelaApagar.removeChild(apagar);
         }
         function criar() {
             //gerando nova table
             for (let cont = 0; cont < contador; cont++) {
                 var linhas = document.createElement('tr');
                 linhas.setAttribute("class", "elemento");
-                tabelaapagar.appendChild(linhas);
+                tabelaApagar.appendChild(linhas);
 
                 for (let cont1 = 0; cont1 < contador; cont1++) {
                     var elementos = document.createElement('td');
@@ -96,7 +70,7 @@ function tamanhoquadrado() {
                 }
             }
             //chamando a função que muda as cores de acordo com o clique
-            adicionarcor();
+            adicionarCor();
         }
     } else {
         alert("O valor passado não está de acordo com o padrão!");
@@ -106,36 +80,36 @@ function tamanhoquadrado() {
 
 //gerando table padrão github
 function github() {
-    limpartela()
+    limparTela()
     let tabelaGit = [];
     let tabelaGitTds = [];
 
-    let elementostr = document.getElementsByTagName('tr');
-    let elementostr2 = elementostr.length - 1;
+    let elementosTr = document.getElementsByTagName('tr');
+    let elementosTr2 = elementosTr.length - 1;
 
-    let contador4 = elementostr2;
+    let contador4 = elementosTr2;
 
-    if (elementostr2 % 2 == 0) {
-        contador4 = elementostr2;
+    if (elementosTr2 % 2 == 0) {
+        contador4 = elementosTr2;
     } else {
-        contador4 = elementostr2 + 1;
+        contador4 = elementosTr2 + 1;
     }
 
-    for (let n = 1; n < elementostr2 + 1; n++) {
+    for (let n = 1; n < elementosTr2 + 1; n++) {
         tabelaGit[n] = document.getElementsByTagName('tr')[n];
 
-        if (elementostr2 > 5) {
+        if (elementosTr2 > 5) {
             for (let j = 0; j < contador4 / 2; j++) {
 
-                let posicao = elementostr2 - j - 1;
+                let posicao = elementosTr2 - j - 1;
 
                 tabelaGitTds[j] = tabelaGit[n].childNodes[j];
                 tabelaGitTds[posicao] = tabelaGit[n].childNodes[posicao];
                 tabelaGitTds[j].value = Math.floor(Math.random() * 2);
 
                 if (tabelaGitTds[j].value == 1) {
-                    tabelaGitTds[j].style.backgroundColor = corfinal;
-                    tabelaGitTds[posicao].style.backgroundColor = corfinal;
+                    tabelaGitTds[j].style.backgroundColor = corFinal;
+                    tabelaGitTds[posicao].style.backgroundColor = corFinal;
                 } else {
                     tabelaGitTds[j].style.backgroundColor = "white";
                     tabelaGitTds[posicao].style.backgroundColor = "white";
@@ -145,14 +119,14 @@ function github() {
         } else {
             for (let i = 1; i < contador4; i = i + 2) {
 
-                posicao = elementostr2 * 2 - i;
+                posicao = elementosTr2 * 2 - i;
                 tabelaGitTds[i] = tabelaGit[n].childNodes[i];
                 tabelaGitTds[posicao] = tabelaGit[n].childNodes[posicao];
                 tabelaGitTds[i].value = Math.floor(Math.random() * 2);
 
                 if (tabelaGitTds[i].value == 1) {
-                    tabelaGitTds[i].style.backgroundColor = corfinal;
-                    tabelaGitTds[posicao].style.backgroundColor = corfinal;
+                    tabelaGitTds[i].style.backgroundColor = corFinal;
+                    tabelaGitTds[posicao].style.backgroundColor = corFinal;
                 } else {
                     tabelaGitTds[i].style.backgroundColor = "white";
                     tabelaGitTds[posicao].style.backgroundColor = "white";
