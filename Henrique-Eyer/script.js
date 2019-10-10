8016f5572e3c180ca0e243bda7c2f0db27cc2e29
Henrique Eyer <henriqueeyer@outlook.com>
2019-10-10 16:38:21 -0300

    Arrumando alguns nomes de variaveis

--- Henrique-Eyer/script.js
+++ Henrique-Eyer/script.js
@@ -11,7 +11,7 @@ var avatar = document.getElementById("avatar");
 window.addEventListener('load', function (){
     corSelect = document.getElementById('color1').style.background;
     disableMarge(document.getElementById('color1')); 
-    create_Matriz(5);
+    createMatriz(5);
 })
 
 avatar.addEventListener('click', function (){
@@ -19,46 +19,46 @@ avatar.addEventListener('click', function (){
 });
 
 function github(){
-    let size_matriz = document.getElementsByTagName('tr');
-    if(size_matriz.length != 0){
-        var mid_of_matriz;
+    let sizeMatriz = document.getElementsByTagName('tr');
+    if(sizeMatriz.length != 0){
+        var midOfMatriz;
         clearAll();
         if(size.value % 2 == 0){
-            mid_of_matriz = size.value/2;
+            midOfMatriz = size.value/2;
         }else{
-            mid_of_matriz = size.value/2+0.5;
+            midOfMatriz = size.value/2+0.5;
         }
-        let line_number;
-        let cell_left;
-        let cell_right = size.value-1;
-        for(line_number = 0; line_number < size.value; line_number++ ){
-            for(cell_left = 0; cell_left < mid_of_matriz; cell_left++, cell_right-- ){
-                let position_cell_left = document.getElementById(line_number+ "," +cell_left)
-                let position_cell_right = document.getElementById(line_number+ "," +cell_right)
-                dye_cell_or_not(position_cell_left, position_cell_right);
+        let lineNumber;
+        let cellLeft;
+        let cellRight = size.value-1;
+        for(lineNumber = 0; lineNumber < size.value; lineNumber++ ){
+            for(cellLeft = 0; cellLeft < midOfMatriz; cellLeft++, cellRight-- ){
+                let positionCellLeft = document.getElementById(lineNumber+ "," +cellLeft)
+                let positionCellRight = document.getElementById(lineNumber+ "," +cellRight)
+                dyeCellOrNot(positionCellLeft, positionCellRight);
                 
             }
-            cell_right = size.value - 1 ;
+            cellRight = size.value - 1 ;
         }
     }
     
 }
 
 
-function dye_cell_or_not(element_left, element_right){
+function dyeCellOrNot(elementLeft, elementRight){
     let random = Math.floor(Math.random() * 2);
     if(random == 1 ){
-        element_left.style.background = corSelect;
-        element_right.style.background = corSelect;
+        elementLeft.style.background = corSelect;
+        elementRight.style.background = corSelect;
     }
 }
 
 size.addEventListener('blur', function(){
-    delete_matriz();
-    create_Matriz(size.value);
+    deleteMatriz();
+    createMatriz(size.value);
 })
 
-function create_Matriz(size){
+function createMatriz(size){
     let line;
     let cell;
     if(size<5){
@@ -74,20 +74,20 @@ function create_Matriz(size){
             linha.appendChild(cel) 
         }
     } 
-    Add_event_click_cells(getMatriz());
+    AddEventClickCells(getMatriz());
     
 }
 
 reset.addEventListener('click', function(){
     
-    delete_matriz();
+    deleteMatriz();
 })
 
-function delete_matriz(){
+function deleteMatriz(){
     let i;
     let element = document.getElementsByTagName('tr');
-    let size_matriz = element.length
-    for( i = 0; i < size_matriz; i++ ){
+    let sizeMatriz = element.length
+    for( i = 0; i < sizeMatriz; i++ ){
         let childtable = table.firstElementChild
         table.removeChild(childtable);
     }
@@ -115,7 +115,7 @@ function disableMarge(item){
     
 }
 
-function gera_color(){
+function randomColor(){
     var hexadecimais = '0123456789ABCDEF';
     var color = '#';
     
@@ -128,14 +128,14 @@ function gera_color(){
 
 for(i = 2; i < 5; i++){
     var colors = document.getElementById('color'+ i);
-    colors.style.background = gera_color();
+    colors.style.background = randomColor();
     colors.addEventListener('click', function(){
         corSelect = this.style.background;
         disableMarge(this);
     })
 }
 
-function Add_event_click_cells(cell){
+function AddEventClickCells(cell){
     for(i = 0; i < cell.length; i++){
         cell[i].addEventListener('click', function (){
             this.style.background = corSelect;
