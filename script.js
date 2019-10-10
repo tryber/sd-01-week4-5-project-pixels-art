88b44a32b8b6518fdce70cedb3433a8d3c6cb802
Mateus Talles <mateustalles@gmail.com>
2019-10-09 22:33:22 -0300

    Código passado para o portugues e função random corrigida

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-var tableArray=[];
+let arrayTabela=[];
 function criaTabela(n) {
-    let tabelaContainer = document.querySelector(".container-table"),
+    let tabelaContainer = document.querySelector(".container-tabela"),
         tabela = document.createElement("table");
-        tabela.className="pixel-table"
+        tabela.className="tabela-pixel"
         tabelaContainer.appendChild(tabela);
         for (let i = 0; i<n ; i++) {
             let linha = document.createElement("tr");
@@ -11,52 +11,52 @@ function criaTabela(n) {
                 let celula = document.createElement("td");
                 let novaLinha = linha.appendChild(celula);
                 novaLinha.id=[i,j];
-                tableArray.push(novaLinha)
+                arrayTabela.push(novaLinha)
             }
         }
-        document.querySelector('.container-table').style.minWidth=n*40+"px";
-        console.log(tableArray)
-        var dimension = n;
+        document.querySelector('.container-tabela').style.minWidth=n*40+"px";
+        console.log(arrayTabela)
+        var dimensao = n;
 }
 
 criaTabela(5)
 
 //atribuindo função à paleta, que armazena a cor numa variavel
-//Bonus 1 - adicionar class "active" para cor ativa
+//Bonus 1 - adicionar class "ativa" para cor ativa
 
     let paletaCor = document.querySelectorAll(".btn-container"),
     corAtual="black";
     for (let cores of paletaCor) {
         console.log(cores.id);
 
-        //configurando classe active ligar e desligar apenas nos eventos corretos, ou seja, quando tem
-        //a classe active-hover e não a active definitiva, que só é setada através do clique.
+        //configurando classe ativa ligar e desligar apenas nos eventos corretos, ou seja, quando tem
+        //a classe ativa-hover e não a ativa definitiva, que só é setada através do clique.
         cores.addEventListener("mouseover", function() {
-            this.className+=" active-hover";
+            this.className+=" ativa-hover";
         });
             cores.addEventListener("mouseleave", function() {
                                                                 //importante o espaço no final dessa class
-                if(this.className.includes("active-hover") && !(this.className.includes("active ")) ) {
+                if(this.className.includes("ativa-hover") && !(this.className.includes("ativa ")) ) {
                 this.className="btn-container";   
                 }         
         }); 
 
         cores.addEventListener("click", function () {
-            let selectedColor = document.querySelector("."+this.id);
-            corAtual = getComputedStyle(selectedColor).backgroundColor;
-            //atribui class active à div pai
+            let corSelecionada  = document.querySelector("."+this.id);
+            corAtual = getComputedStyle(corSelecionada).backgroundColor;
+            //atribui class ativa à div pai
             
-            for (each of paletaCor) {
-                each.className="btn-container";
+            for (cor of paletaCor) {
+                cor.className="btn-container";
             }
-            this.className+=" active";
+            this.className+=" ativa";
         })
     }
 
 //atribuindo função de pintar a tabela de pixel
 
-    for (pixel of tableArray) {
-        console.log(pixel)
+    for (pixel of arrayTabela) {
+      //  console.log(pixel)
         pixel.addEventListener("click", function() {
             this.style.backgroundColor=corAtual;
         })
@@ -65,22 +65,22 @@ criaTabela(5)
     //resetar quadro - Bonus #2
     let reset = document.querySelector(".reset");
     reset.addEventListener("click",function() {
-        for (cells of tableArray) {
-            cells.style.backgroundColor="white";
+        for (pixel of arrayTabela) {
+            pixel.style.backgroundColor="white";
         }
     });
 
     function resetar() {
-        for (cells of tableArray) {
-            cells.style.backgroundColor="white";
+        for (pixel of arrayTabela) {
+            pixel.style.backgroundColor="white";
         }
     };
         
     //setar tamanho customizado: Bonus #3
-    let inputSize = document.querySelector(".input-tablesize");
-    inputSize.addEventListener("change", function() {
-        tableArray=[];
-        document.querySelector(".pixel-table").remove();
+    let inputTamanho = document.querySelector(".input-tamanho");
+    inputTamanho.addEventListener("change", function() {
+        arrayTabela=[];
+        document.querySelector(".tabela-pixel").remove();
         criaTabela(this.value);
     });
 
@@ -88,16 +88,16 @@ criaTabela(5)
 
     window.addEventListener("load", function(){
         let paletaCor = document.querySelectorAll(".btn-container");
-        for (pixels of tableArray) {
-            for (let colors of paletaCor) {
-                //Este comando exclui o ultimo digito do colors.id que é um número, e adicionada
+        for (pixels of arrayTabela) {
+            for (let cor of paletaCor) {
+                //Este comando exclui o ultimo digito do cor.id que é um número, e adicionada
                 //a string "color" um outronumero aleatoriamente gerado de 0 a 3, 
                 //e depois adiciona +1, pois os Id's estão
                 //numeros como color1, color2, color3 e color 4;                
-            let idRandom = ((colors.id).slice(0,-1)) + (Math.floor(Math.random() * 4)+1) ;
+            let idRandom = ( cor.id.slice(0,-1)) + (Math.floor(Math.random() * 4)+1) ;
             //captura o valor de backgroundColor das divs de cada botão
-            let selectedColor = document.querySelector("."+idRandom);
-            corAtual = getComputedStyle(selectedColor).backgroundColor;
+            let corSelecionada = document.querySelector("."+idRandom);
+            corAtual = getComputedStyle(corSelecionada).backgroundColor;
             //aplica a cada pixel que esteja endo itnerpolado.            
             pixels.style.backgroundColor=corAtual;
             }
@@ -108,16 +108,17 @@ criaTabela(5)
 
     let btnAlien = document.querySelector(".btn-github");
     btnAlien.addEventListener("click", function (){
-    let tableLength = Math.sqrt(tableArray.length),
-    halfTable = tableLength/2;
+    let tamanhoTabela = Math.sqrt(arrayTabela.length),
+    metadeTabela = tamanhoTabela/2;
         //Esta seção é responsável por "espelhar" o quadro.
-        for (let x = 0; x<tableLength ; x++) {
-            let alienY = Math.floor(Math.random()*(tableLength-halfTable));
+        for (let x = 0; x<tamanhoTabela ; x++) {
+            let alienY = Math.floor(Math.random()*(tamanhoTabela-metadeTabela));
             let alienPixel = document.getElementById(x+","+alienY);
             alienPixel.style.backgroundColor=corAtual;
-            let antiY=tableLength-1-alienY;
+            let antiY=tamanhoTabela-1-alienY;
             let antiPixel = document.getElementById(x+","+antiY);
             console.log(antiPixel);
             antiPixel.style.backgroundColor=corAtual;
             }
         });
+        
