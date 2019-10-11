3dbbe3ffe7e3f8cf32223c403186cad67b6ef6a2
Gabriel Dalseco dos Santos <gabriel_santos96@outlook.com>
2019-10-10 22:35:57 -0300

    Correções realizadas

--- coruja2/scripts.js
+++ coruja2/scripts.js
@@ -1,39 +1,38 @@
-var color1 = document.getElementsByClassName("paleta1")[0]
-var color2 = document.getElementsByClassName("paleta2")[0]
-var color3 = document.getElementsByClassName("paleta3")[0]
-var color4 = document.getElementsByClassName("paleta4")[0]
+let color1 = document.getElementById("paleta1");
+let color2 = document.getElementById("paleta2");
+let color3 = document.getElementById("paleta3");
+let color4 = document.getElementById("paleta4");
 
-var selectColor = "black"
+let selectColor = "black";
 pintartodasascores();
 
-color1.addEventListener('click', function(){
+color1.addEventListener('click', function() {
   let computedStyle = getComputedStyle(color1);
   selectColor = computedStyle.backgroundColor;
 })
 
-color2.addEventListener('click', function(){
+color2.addEventListener('click', function() {
   let computedStyle = getComputedStyle(color2);
   selectColor = computedStyle.backgroundColor;
 })
 
-color3.addEventListener('click', function(){
+color3.addEventListener('click', function() {
   let computedStyle = getComputedStyle(color3);
   selectColor = computedStyle.backgroundColor;
 })
 
-color4.addEventListener('click', function(){
+color4.addEventListener('click', function() {
   let computedStyle = getComputedStyle(color4);
   selectColor = computedStyle.backgroundColor; 
 })
 
-function pintartodasascores(){
-  let printando = document.getElementsByTagName("td")
-  for (let i = 0; i < printando.length ;i++){
-    printando[i].addEventListener('click', function(){
-    printando[i].style.background = selectColor;
+function pintartodasascores() {
+  let printando = document.getElementsByTagName("td");
+  for (let recebendo of printando) {
+    recebendo.addEventListener('click', function(){
+      recebendo.style.background = selectColor;
     })  
   }
 }
-  
-  
-  
+
+
