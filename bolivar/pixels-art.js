e606b930513f5818e24a30bfd63fc6f35a415e42
Anderson Bolivar <bolivar.anderson@hotmail.com>
2019-10-11 12:44:22 -0300

    Review Work 80% Done

--- bolivar/pixels-art.js
+++ bolivar/pixels-art.js
@@ -1,24 +1,14 @@
-var count = 0;
-function tableSize(){
-    count += 1;
-    var row = document.createElement("tbody");
-    var subtittle = document.getElementById("subtittle")
-    subtittle.innerHTML = count + "x" + count 
-    row.id = "row" + count;
-    document.getElementById("table-pixel-canvas").appendChild(row);
-    for (i = 1; i <= count; i++){
-        var select = document.getElementById("row" + i)
-        var limit = select.childElementCount;
-        for (l = 0; l < (count - limit); l++){
-            collum = document.createElement("th");
-            collum.className = "collum";
-            collum.id = "collum" + i + (l + limit + 1)
-            select.appendChild(collum);
-        }
-    }
+// Search Elements in the html File
+function searchElements() {
+    var palette = document.getElementById("collors-Pallete");
+    var input = document.getElementById("input-canvas-size");
+    var canvas = document.getElementById("table-pixel-canvas");
+    var buttonCanvasSize = document.getElementById("canvas-size")
+    return [palette, input, canvas, buttonCanvasSize]
 }
 
-function setcollor(){
+// Set a random collor 
+function setcollor() {
     var one = Math.floor(Math.random() * 256);
     var two = Math.floor(Math.random() * 256);
     var three = Math.floor(Math.random() * 256);
@@ -26,84 +16,213 @@ function setcollor(){
     return collor
 }
 
-function changeCollor(){
-    for (i = 1; i <= 10; i++){
-        var div = document.getElementById("div" + i)
-        div.style.backgroundColor= setcollor()
-        div.style.width = "60px"
-        div.style.height = "60px"
+// Change Palette's Size // Each time you click the button return 4 colors
+clickcountaddColors = -1;
+function addColors() {
+    clickcountaddColors += 1;
+    var element = searchElements();
+    if (clickcountaddColors > 15) {
+        clickcountaddColors = 0;
+        var limit = element[0].childElementCount;
+        for(i = 1; i < limit; i++) {
+            var color = document.getElementById("color4" + i)
+            element[0].removeChild(color)
+        }
     }
-    
+    else if (clickcountaddColors == 0){
+        var colorFour =  document.createElement("div");
+        colorFour.id = "color4" + clickcountaddColors;
+        colorFour.style.display = "flex";
+        colorFour.style.justifyContent = "center";
+        element[0].appendChild(colorFour);
+        var color4 = document.getElementById("color4" + clickcountaddColors);
+        for (i = 0; i < 4; i++) {
+            var color = document.createElement("div");
+            color4.appendChild(color);
+            color.id = "color" + (i + clickcountaddColors*4);
+            color.className = "color";
+            if (i == 0){
+                color.style.backgroundColor = "black";
+            }
+            else {
+                color.style.backgroundColor = setcollor();
+            }
+        }
+    }
+    else {
+        var colorFour =  document.createElement("div");
+        colorFour.id = "color4" + clickcountaddColors;
+        colorFour.style.display = "flex";
+        colorFour.style.justifyContent = "center";
+        element[0].appendChild(colorFour);
+        var color4 = document.getElementById("color4" + clickcountaddColors);
+        for (i = 0; i < 4; i++) {
+            var color = document.createElement("div");
+            color4.appendChild(color);
+            color.id = "color" + (i + clickcountaddColors*4);
+            color.className = "color";
+            color.style.backgroundColor = setcollor();
+        }
+    }
+    pickPaletteColor();
 }
 
-function divClick(){
-    for (i = 1; i <= 10; i++){
-        var div = document.getElementById("div" + i)
-        div.addEventListener("click", function() {
-            var collor = this.style.backgroundColor
-            divPaint(collor)
-        })
+// Change Palette's Colors
+function changeCollor() {
+    var element = searchElements();
+    for (i = 0; i < element[0].childElementCount; i++){
+        for (l = 0; l < element[0].childNodes[i].childElementCount; l++){
+            var color = element[0].childNodes[i].childNodes[l];
+            color.style.backgroundColor = setcollor();
+        }
     }
-    var black = document.getElementById("black")
-    black.addEventListener("click", function(){
-        divPaint("black")
-    })
+    element[0].childNodes[0].childNodes[0].style.backgroundColor = "black";
 }
-function divPaint(collor){
-    for (i = 1; i <= (document.getElementById("table-pixel-canvas").childElementCount - 1); i++ ){
-        for (l = 1; l <= (document.getElementById("table-pixel-canvas").childElementCount - 1); l++){
-            var element = document.getElementById("collum" + i + l)
-            element.addEventListener("click", function(){
-                this.style.backgroundColor = collor;
-                
+
+// Canvas Size
+function canvasSize(init) {
+    cleanCanvas();
+    var element = searchElements();
+    if (init == 5){
+        var inputvalue = 5;
+    }
+    else {
+        var inputvalue = parseInt(element[1].value);
+    }
+    if (inputvalue == NaN){
+        alert("Select a valid value, please!");
+    }
+    else {
+        if (inputvalue > 20){
+            alert("20 is the maximum size");
+        }
+        else{
+            for (i = 0; i < inputvalue; i++){
+                var row = document.createElement("tbody");
+                row.id = "row" + i;
+                row.className = "row";
+                for (l = 0; l < inputvalue; l++){
+                    var pixel = document.createElement("td");
+                    pixel.id = "pixel" + i + l;
+                    pixel.className = "pixel";
+                    row.appendChild(pixel);
+                }
+                element[2].appendChild(row);
+            }
+        }
+    }   
+}
+
+// Erase Exist Canvas
+function cleanCanvas() {
+    var element = searchElements();
+    for (i = 0; element[2].childElementCount; i ++){
+        element[2].removeChild(element[2].childNodes[i]);
+        i = -1;
+    }
+}    
+   
+// Pick the palette color
+var cor = new String();
+var countclick = 0;
+function pickPaletteColor() {
+    var element = searchElements();
+    for (i = 0; i < element[0].childElementCount; i++){
+        for (l = 0; l < element[0].childNodes[i].childElementCount; l++){
+            element[0].childNodes[i].childNodes[l].addEventListener("click", function() {
+                var collor = this.style.backgroundColor
+                paintCanvas(collor);
+                cor = collor;
+                countclick += 1;
+                if ((countclick == 1) && (this.id !== "selected-item")){
+                    reviewSelect()
+                    this.id = "selected-item";
+                }
+                else if ((countclick == 1) && (this.id == "selected-item")){
+                    reviewSelect();
+                }   
+                else if ((countclick == 2) && (this.id !== "selected-item")){
+                    reviewSelect();
+                    this.id = "selected-item"
+                    countclick = 0;
+                }
+                else if ((countclick == 2) && (this.id == "selected-item")){
+                    reviewSelect();
+                    countclick = 0;
+                }
             })
         }
     }
 }
 
-function tableErase(){
-    for (i = 1; i <= (document.getElementById("table-pixel-canvas").childElementCount - 1); i++ ){
-        for (l = 1; l <= (document.getElementById("table-pixel-canvas").childElementCount - 1); l++){
-            var element = document.getElementById("collum" + i + l)
-            element.style.backgroundColor = "white";
+// Clean the other select color
+var countReviw = -1;
+function reviewSelect(){
+    var element = searchElements();
+    for (i = 0; i < element[0].childElementCount; i++){
+        countReviw += 1;
+        for (l = 0; l < element[0].childNodes[i].childElementCount; l++){
+            element[0].childNodes[i].childNodes[l].id = "color" + (l +  countReviw*4);
+        }
+    }
+    countReviw = -1;
+}
+
+// Paint Canvas
+function paintCanvas(collor) {
+    var element = searchElements();
+    for (i = 0; i < element[2].childElementCount; i++){
+        for (l = 0; l < element[2].childNodes[i].childElementCount; l++){
+            element[2].childNodes[i].childNodes[l].addEventListener("click", function() {
+                this.style.backgroundColor = collor;
+            })
         }
     }
 }
 
-
-function party(){
-    var queryblock = "collum" + (Math.floor(Math.random() * (document.getElementById("table-pixel-canvas").childElementCount - 1))+1) + (Math.floor(Math.random() * (document.getElementById("table-pixel-canvas").childElementCount - 1))+ 1)
-    document.getElementById(queryblock).style.backgroundColor = setcollor();
-}
-
-var math = 0;
-function perfil(){
-    math += 1
-    var queryblock = "collum" + (Math.floor(Math.random() * (document.getElementById("table-pixel-canvas").childElementCount - 1))+1) + (Math.floor(Math.random() * (document.getElementById("table-pixel-canvas").childElementCount - 1))+ 1)
-    if (math == 1){
-        var arm = document.createElement("div")
-        arm.id = "fashion"
-        arm.style.backgroundColor = setcollor();
-        document.body.appendChild(arm);
-        document.getElementById(queryblock).style.backgroundColor = document.getElementById("fashion").style.backgroundColor
-    }
-    else {
-        document.getElementById(queryblock).style.backgroundColor = document.getElementById("fashion").style.backgroundColor
+// Erase Button 
+function cleanCanvasCollors() {
+    var element = searchElements();
+    for (i = 0; i < element[2].childElementCount; i++){
+        for (l = 0; l < element[2].childNodes[i].childElementCount; l++){
+            element[2].childNodes[i].childNodes[l].style.backgroundColor = "white";
+        }
     }
 }
 
-function background(){
-    document.getElementById("main-title").style.backgroundColor = setcollor();
-    for(i = 0; i < document.getElementsByClassName("subtitles").length; i++){
-        document.getElementsByClassName("subtitles")[i].style.backgroundColor = setcollor();
+// Random Colors to Canvas
+function party() {
+    var element = searchElements();
+    for (i = 0; i < element[2].childElementCount; i ++){
+        for (l = 0; l < element[2].childNodes[i].childElementCount; l++){
+            element[2].childNodes[i].childNodes[l].style.backgroundColor = setcollor();
+        }
     }
-    for(i = 0; i < document.getElementsByClassName("buttonbottom").length; i++){
-        document.getElementsByClassName("buttonbottom")[i].style.backgroundColor = setcollor();
+}
+
+// Random Colors to Random Pixels
+function fillColors (){
+    var element = searchElements();
+    var randomNumRow = Math.floor(Math.random()*element[2].childElementCount);
+    var randomNumPixel = Math.floor(Math.random()*element[2].childElementCount);
+    element[2].childNodes[randomNumRow].childNodes[randomNumPixel].style.backgroundColor = setcollor();
+}
+
+// Random Profile max 70% of the canvas min 30% of the canvas
+function profile(){
+    cleanCanvasCollors()
+    var element = searchElements();
+    var area = element[2].childElementCount * element[2].childElementCount;
+    var randomNum = Math.floor((Math.random()*(area*0.4)) + (area*0.3));
+    for (i = 0; i < randomNum; i++){
+        var randomNumRow = Math.floor(Math.random()*element[2].childElementCount);
+        var randomNumPixel = Math.floor(Math.random()*element[2].childElementCount);
+        element[2].childNodes[randomNumRow].childNodes[randomNumPixel].style.backgroundColor = cor;
     }
-    document.getElementById("subtittle").style.backgroundColor = setcollor();
 }
 
 window.onload = function () {
-    divClick()
-    background()
+    addColors(); // Set the firs 4 colors
+    canvasSize(5); // Display a Canvas 5X5 , 25 pixels;
+    pickPaletteColor() // Set click events on Palette
 } 
